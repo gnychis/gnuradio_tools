7f1d75341aef7694f1e0604f4aa9928afddcfb8f
gnychis <gnychis@14b32a93-68d6-dc11-9d15-00e08143843c>
2008-03-27 01:06:27 +0000

    building TMAC

---
+++ macs/cmusdrg_macs/branches/tmac/src/lib/tmac.cc
@@ -0,0 +1,269 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2007 Free Software Foundation, Inc.
+ * 
+ * This file is part of GNU Radio
+ * 
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <tmac.h>
+
+static bool verbose = false;
+
+static pmt_t s_timeout = pmt_intern("%timeout");
+
+tmac::tmac(mb_runtime *rt, const std::string &instance_name, pmt_t user_arg)
+  : mac(rt, instance_name, user_arg),
+  d_state(INIT_TMAC)
+{
+  define_mac_ports();   // Initialize ports for message passing
+  d_local_address = pmt_to_long(pmt_nth(1, user_arg));
+}
+
+tmac::~tmac()
+{
+}
+
+void tmac::define_mac_ports()
+{
+  // Create a small dictionary to pass some information to the PHY
+  pmt_t phy_dict = pmt_make_dict();
+  pmt_dict_set(phy_dict, pmt_intern("interp-tx"), pmt_from_long(d_usrp_interp));
+  pmt_dict_set(phy_dict, pmt_intern("decim-rx"), pmt_from_long(d_usrp_decim));
+
+  // Connect to physical layer
+  define_component("GMSK", "gmsk", phy_dict);
+  d_gmsk_cs = define_port("phy-cs", "gmsk-cs", false, mb_port::INTERNAL);
+  connect("self", "phy-cs", "GMSK", "cs0");
+
+  // Define ports for the application to connect to us
+  d_tx = define_port("tx0", "tmac-tx", true, mb_port::EXTERNAL);  // Transmit
+  d_rx = define_port("rx0", "tmac-rx", true, mb_port::EXTERNAL);  // Receive
+  d_cs = define_port("cs", "tmac-cs", true, mb_port::EXTERNAL);   // Control/Status
+}
+
+// Invoked when the base 'mac' class finishes initializing the USRP
+void tmac::usrp_initialized()
+{
+  initialize_tmac();
+}
+
+void tmac::initialize_tmac()
+{
+  d_state = IDLE;   // State where we wait for messages to do something
+  
+  d_cs->send(s_response_tmac_initialized,   // Notify the application that
+             pmt_list2(PMT_NIL, PMT_T));    // the MAC is initialized
+
+  if(verbose)
+    std::cout << "[TMAC] Initialized, and idle\n";
+}
+
+// This is the crux of the MAC layer.  The general architecture is to have
+// states at which the MAC is in, and then handle messages from the application
+// and physical layer based on that state.  
+// 
+// Then, the incoming 'event' (type of message) in the state triggers some
+// functionality.  For example, if we are in the idle state and receive a 
+// s_cmd_tx_pkt event from the application (detected by the port ID), we send
+// the data to the physical layer to be modulated.
+//
+// Without the m-block to gr-block connection, all raw samples are forced
+// through the MAC layer.  So, in the idle state we receive raw incoming
+// samples, pass them to the physical layer for demodulation, and listen for
+// responses with frames.  Likewise, we send data to the modulator, get
+// modulated data back, and then we write it to the USRP.
+void tmac::handle_mac_message(mb_message_sptr msg)
+{
+  pmt_t event = msg->signal();      // type of message
+  pmt_t data = msg->data();         // the associated data
+  pmt_t port_id = msg->port_id();   // the port the msg was received on
+
+  std::string error_msg;
+
+  switch(d_state) {
+    
+    //----------------------------- INIT TMAC --------------------------------//
+    // In the INIT_TMAC state, now that the USRP is initialized.
+    case INIT_TMAC:
+      goto unhandled;
+
+    //----------------------------- WAIT SYNC -------------------------------//
+    // In this state, we're waiting for a synchronization frame from the
+    // basestation so that we can aligned our round and slot.
+    case WAIT_SYNC:
+      goto unhandled;
+    
+    //----------------------------- IDLE ------------------------------------//
+    // In the idle state the MAC is not quite 'idle', it is just not doing
+    // anything specific.  It is still being passive with data between the
+    // application and the lower layer.
+    case IDLE:
+      
+      //---- Port: TMAC TX -------------- State: IDLE -----------------------//
+      if(pmt_eq(d_tx->port_symbol(), port_id)) {
+
+        if(pmt_eq(event, s_cmd_tx_pkt)) {
+          d_gmsk_cs->send(s_cmd_mod, data);           // Modulate the data
+        }
+        return;
+      }
+      
+      //---- Port: TMAC CS -------------- State: IDLE -----------------------//
+      if(pmt_eq(d_cs->port_symbol(), port_id)) {
+
+        if(pmt_eq(event, s_cmd_rx_enable)) {
+          enable_rx();                                // Enable RX
+        }
+        else if(pmt_eq(event, s_cmd_rx_disable)) {
+          disable_rx();                               // Disable RX
+        }
+        return;
+      }
+
+      //---- Port: USRP TX -------------- State: IDLE -----------------------//
+      if(pmt_eq(d_us_tx->port_symbol(), port_id)) {
+
+        if(pmt_eq(event, s_response_xmit_raw_frame)) {
+          packet_transmitted(data);                   // Transmission successful
+        }
+        return;
+      }
+      
+      //---- Port: USRP RX -------------- State: IDLE -----------------------//
+      if(pmt_eq(d_us_rx->port_symbol(), port_id)) {
+
+        if(pmt_eq(event, s_response_recv_raw_samples)) {
+          d_gmsk_cs->send(s_cmd_demod, data);         // Demod incoming samples
+        }
+        return;
+      }
+
+      //---- Port: GMSK CS -------------- State: IDLE -----------------------//
+      if(pmt_eq(d_gmsk_cs->port_symbol(), port_id)) {
+        
+        if(pmt_eq(event, s_response_mod)) {
+          transmit_pkt(data);                         // Data done being mod'ed
+        }
+        else if(pmt_eq(event, s_response_demod)) {
+          incoming_frame(data);                       // Incoming frame!
+        }
+        return;
+      }
+      goto unhandled;
+
+  } // End of switch()
+  
+ // Received an unhandled message for a specific state
+ unhandled:
+  if(0 && verbose && !pmt_eq(event, pmt_intern("%shutdown")))
+    std::cout << "[TMAC] unhandled msg: " << msg
+              << "in state "<< d_state << std::endl;
+}
+
+// Handles the transmission of a pkt from the application.  The invocation
+// handle is passed on but a response is not given back to the application until
+// the response is passed from usrp_server.  This ensures that the MAC passes
+// back the success or failure. 
+void tmac::transmit_pkt(pmt_t data)
+{
+  pmt_t invocation_handle = pmt_nth(0, data);
+  pmt_t samples = pmt_nth(1, data);
+  pmt_t pkt_properties = pmt_nth(2, data);
+
+  // A dictionary (a hash like structure) that is used to pass packet properties
+  // down the layers.
+  pmt_t us_tx_properties = pmt_make_dict();
+
+  pmt_t timestamp = pmt_from_long(0xffffffff);	// 0xffffffff == transmit NOW!
+
+  pmt_t pdata = pmt_list5(invocation_handle,    // Invocation handle is passed back.
+		                      d_us_tx_chan,         // Destined for our TX channel.
+		                      samples,              // The modulated data (samples).
+                          timestamp,            // The time to send the packet.
+                          us_tx_properties);    // Our per-packet properties.
+
+  d_us_tx->send(s_cmd_xmit_raw_frame, pdata);   // Finally, send!
+
+  if(verbose && 0)
+    std::cout << "[TMAC] Transmitted packet\n";
+}
+
+// Invoked when we get a response that a packet was written to the USRP USB bus,
+// we assume that it has been transmitted (or will be, within negligable time).
+//
+// The application is NOT informed that the packet was transmitted successfully
+// until an ACK is received, so we enter an ACK wait state.
+void tmac::packet_transmitted(pmt_t data)
+{
+  pmt_t invocation_handle = pmt_nth(0, data);
+  pmt_t status = pmt_nth(1, data);
+
+  enable_rx();  // Need to listen for ACKs
+  
+  d_state = IDLE;
+}
+
+// An incoming frame from the physical layer for us!  We check the packet
+// properties to determine the sender and if it passed a CRC check, for example.
+void tmac::incoming_frame(pmt_t data)
+{
+  pmt_t invocation_handle = PMT_NIL;
+  pmt_t payload = pmt_nth(0, data);
+  pmt_t pkt_properties = pmt_nth(1, data);
+
+  // Let's do some checking on the demoded frame
+  long lsrc=0, ldst=0;
+  bool b_crc = false;
+
+  // Properties are set in the physical layer framing code
+  lsrc = pmt_to_long(pmt_dict_ref(pkt_properties,
+                                  pmt_intern("src"),
+                                  PMT_NIL));
+
+  ldst = pmt_to_long(pmt_dict_ref(pkt_properties,
+                                  pmt_intern("dst"),
+                                  PMT_NIL));
+
+  if(pmt_t crc = pmt_dict_ref(pkt_properties,
+                              pmt_intern("crc"),
+                              PMT_NIL)) {
+    if(pmt_eqv(crc, PMT_T))
+      b_crc = true;   // payload passed CRC
+    else
+      b_crc = false;  // payload failed CRC
+  }
+
+  if(ldst != d_local_address)  // not for this address
+    return;
+  
+  d_rx->send(s_response_rx_pkt, pmt_list3(invocation_handle, payload, pkt_properties));
+
+  if(verbose)
+    std::cout << "[TMAC] Passing up demoded frame\n";
+}
+
+void tmac::incoming_sync(pmt_t data)
+{
+
+}
+
+
+REGISTER_MBLOCK_CLASS(tmac);
---
+++ macs/cmusdrg_macs/branches/tmac/src/lib/tmac.h
@@ -0,0 +1,99 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2007 Free Software Foundation, Inc.
+ * 
+ * This file is part of GNU Radio
+ * 
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef INCLUDED_TMAC_H
+#define INCLUDED_TMAC_H
+
+#include <mb_mblock.h>
+#include <mb_runtime.h>
+#include <mb_protocol_class.h>
+#include <mb_exception.h>
+#include <mb_msg_queue.h>
+#include <mb_message.h>
+#include <mb_msg_accepter.h>
+#include <mb_class_registry.h>
+#include <pmt.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <ui_nco.h>
+
+#include <symbols_usrp_server_cs.h>
+#include <symbols_usrp_channel.h>
+#include <symbols_usrp_low_level_cs.h>
+#include <symbols_usrp_tx.h>
+#include <symbols_usrp_rx.h>
+
+#include <tmac_symbols.h>
+#include <gmsk_symbols.h>
+
+#include <mac.h>
+
+class tmac;
+
+class tmac : public mac
+{
+
+  enum tmac_state_t {
+    INIT_TMAC,
+    WAIT_SYNC,
+    IDLE,
+  };
+  tmac_state_t	d_state;
+
+  // Ports used for applications to connect to this block
+  mb_port_sptr		  d_tx, d_rx, d_cs;
+
+  // Ports to connect to gmsk (us)
+  mb_port_sptr      d_gmsk_cs;
+
+  // TDMA state
+  unsigned long d_last_sync;
+  unsigned long d_slot_time;
+  unsigned long d_guard_time;
+  unsigned long d_local_slot_offset;
+  unsigned long d_next_tx_time;
+
+  // Local user address
+  long d_local_address;
+  
+ protected:
+  void handle_mac_message(mb_message_sptr msg);   // overriding MAC method
+  void usrp_initialized();                        // overriding MAC method
+
+ public:
+  tmac(mb_runtime *rt, const std::string &instance_name, pmt_t user_arg);
+  ~tmac();
+
+ private:
+  // TMAC initialization
+  void define_mac_ports();
+  void initialize_tmac();
+
+  // Crucial CSMA methods
+  void transmit_pkt(pmt_t data);
+  void packet_transmitted(pmt_t data);
+  void incoming_frame(pmt_t data);
+  void incoming_sync(pmt_t data);
+  
+};
+
+#endif // INCLUDED_TMAC_H
