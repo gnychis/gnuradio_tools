808e84f6406e7358c27b7b240e0f9ab038f28d80
gnychis <gnychis@14b32a93-68d6-dc11-9d15-00e08143843c>
2008-04-14 18:57:06 +0000

    new program to read short

---
+++ tools/read_short_complex.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <complex>
+
+typedef int16_t data_t;
+
+int main(int argc, char *argv[]) {
+
+  if(argc != 2) {
+    std::cout << "Usage: ./read_complex <data_file>\n";
+    return -1;
+  }
+
+  std::ifstream infile;
+
+  infile.open(argv[1], std::ios::binary|std::ios::in);
+  if(!infile.is_open())
+    exit(-1);
+    
+  data_t real, imag;
+
+  infile.read((char *)&real, sizeof(data_t));
+  infile.read((char *)&imag, sizeof(data_t));
+
+  while(!infile.eof()) {
+    printf("%d %d\n", real, imag);
+    infile.read((char *)&real, sizeof(data_t));
+    infile.read((char *)&imag, sizeof(data_t));
+  }
+
+  infile.close();
+}
