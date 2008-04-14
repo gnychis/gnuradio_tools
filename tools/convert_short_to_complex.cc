cb8b5e8040f8ea685542e50a5bedf9923d7ed9ee
gnychis <gnychis@14b32a93-68d6-dc11-9d15-00e08143843c>
2008-04-14 21:49:27 +0000

    trying to write conversion script

---
+++ tools/convert_short_to_complex.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <complex>
+
+typedef int16_t data_t;
+typedef std::complex<float> data2_t;
+
+int main(int argc, char *argv[]) {
+
+  if(argc != 3) {
+    std::cout << "Usage: ./read_complex <data_file> <out>\n";
+    return -1;
+  }
+
+  std::ifstream infile;
+  std::ofstream ofile;
+
+  infile.open(argv[1], std::ios::binary|std::ios::in);
+  ofile.open(argv[2], std::ios::binary|std::ios::out);
+  if(!infile.is_open() || !ofile.is_open())
+    exit(-1);
+    
+  data_t real, imag;
+  data2_t comp;
+
+  infile.read((char *)&real, sizeof(data_t));
+  infile.read((char *)&imag, sizeof(data_t));
+  comp = data2_t(real,imag);
+  ofile.write((const char *)&comp, sizeof(comp));
+
+
+  while(!infile.eof()) {
+    printf("%d %d\n", real, imag);
+    infile.read((char *)&real, sizeof(data_t));
+    infile.read((char *)&imag, sizeof(data_t));
+    comp = data2_t(real,imag);
+    ofile.write((const char *)&comp, sizeof(comp));
+  }
+
+  infile.close();
+  ofile.close();
+}
