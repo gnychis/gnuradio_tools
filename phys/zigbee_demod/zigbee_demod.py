b4d65c1c319adfda2f8b3d463088b8e5cfef9503
gnychis <gnychis@14b32a93-68d6-dc11-9d15-00e08143843c>
2008-04-26 17:18:49 +0000

    reading sync coeffs now

--- phys/zigbee_demod/zigbee_demod.py
+++ phys/zigbee_demod/zigbee_demod.py
@@ -27,7 +27,7 @@ from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio import cmusdrg
 
-def build_graph(input, output, coeffs, sync_thresh, sync_window):
+def build_graph(input, output, acq_coeffs, sync_coeffs, sync_thresh, sync_window):
 
   # Initialize our top block
   fg = gr.top_block()
@@ -35,14 +35,22 @@ def build_graph(input, output, coeffs, sync_thresh, sync_window):
   # Source is a file
   src = gr.file_source (gr.sizeof_gr_complex, input)
 
-  # Read coefficients for the matched filter (FIR filter)
-  dfile = open(coeffs, 'r')
+  # Read coefficients for the acquisition filter (FIR filter)
+  dfile = open(acq_coeffs, 'r')
   data = []
   for line in dfile:
     data.append(complex(*map(float,line.strip().strip("()").split(" "))).conjugate())
   dfile.close()
   data.reverse()
   mfilter = gr.fir_filter_ccc(1, data)    # Our matched filter!
+  
+  # Read coefficients for the sync block
+  dfile = open(sync_coeffs, 'r')
+  data = []
+  for line in dfile:
+    data.append(complex(*map(float,line.strip().strip("()").split(" "))).conjugate())
+  dfile.close()
+  sync = gr.mf_sync_ccf(data)    # Sync block!
 
   # Delay component, to sync the original complex with MF output
   delay = gr.delay(gr.sizeof_gr_complex, len(data)-1)
@@ -58,29 +66,30 @@ def build_graph(input, output, coeffs, sync_thresh, sync_window):
   fg.connect(mfilter, (acq, 0))
   fg.connect(delay, (acq, 1))
 
+  # Connect the acquisition filter to the sync block
+  fg.connect((acq, 0), (sync, 0))
+  fg.connect((acq, 1), (sync, 1))
+
   # Two file sinks for the output
-  fsink1 = gr.file_sink (gr.sizeof_gr_complex, output+"_acq")
-  fsink2 = gr.file_sink (gr.sizeof_gr_complex, output+"_complex")
-  fsink3 = gr.file_sink (gr.sizeof_gr_complex, output+"_mfilter")
-  fg.connect((acq, 0), fsink1)
-  fg.connect((acq, 1), fsink2)
-  fg.connect(mfilter, fsink3)
+  fsink = gr.file_sink (gr.sizeof_float, output+"_sync")
+  fg.connect(sync, fsink)
 
   return fg
 
 def main (args):
   nargs = len (args)
-  if nargs == 5:
+  if nargs == 6:
       input = args[0]
       output = args[1]
-      coeffs = args[2]
-      sync_thresh = int(args[3])
-      sync_window = int(args[4])
+      acq_coeffs = args[2]
+      sync_coeffs = args[3]
+      sync_thresh = int(args[4])
+      sync_window = int(args[5])
   else:
-      sys.stderr.write ("usage: ./zigbee_demod.py input output coeffs sync_thresh sync_window\n")
+      sys.stderr.write ("usage: ./zigbee_demod.py input output acq_coeffs sync_coeffs sync_thresh sync_window\n")
       sys.exit (1)
 
-  fg = build_graph (input, output, coeffs, sync_thresh, sync_window)
+  fg = build_graph (input, output, acq_coeffs, sync_coeffs, sync_thresh, sync_window)
   fg.run()
 
 if __name__ == '__main__':
