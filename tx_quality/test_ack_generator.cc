bad6c8205127d209a45ae995146d1aa30ead8333
gnychis <gnychis@14b32a93-68d6-dc11-9d15-00e08143843c>
2008-05-07 19:13:41 +0000

    building, need to compute sdev

--- tx_quality/test_ack_generator.cc
+++ tx_quality/test_ack_generator.cc
@@ -6,11 +6,51 @@
 #include <map>
 #include <fstream>
 
+const int tsamples=23872;
+
+// State
+bool first;
+bool monitoring;
+long samples_left;
+long ntransmission;
+
+// Stats
+unsigned long avg, min, max;
+unsigned long long sum;
+
+void compute()
+{
+  bool ack;
+
+  if(first) {
+    std::cout << ntransmission << "1";
+    first=false;
+    return;
+  }
+
+  avg=sum/tsamples;
+
+}
+
+void handle(unsigned long power)
+{
+  if(!monitoring && first) {
+    monitoring=true;
+    samples_left=tsamples;
+  }
+
+  sum+=power;
+  samples_left--;
+
+  if(samples_left==0)
+    compute();
+}
+
 int main(int argc, char **argv) {
 
   std::string curr_line;
 
-  getline(std::cin, curr_line);     // skip the header description line in flow-export
+  first=true;
 
   // Input format:
   //   src_ip dst_ip src_port dst_port src_pkts dst_pkts
@@ -22,15 +62,18 @@ int main(int argc, char **argv) {
     if(curr_line=="") break;
 
     std::vector<std::string> tokens;
-    for(std::string each; std::getline(in,each,','); tokens.push_back(each));
+    for(std::string each; std::getline(in,each,'\t'); tokens.push_back(each));
 
     // Convert the ports to integers
-    float flag, power;
+    unsigned long mf_flag, power;
     std::istringstream mf(tokens[0]), pow(tokens[1]);
-    mf >> flag;
+    mf >> mf_flag;
     pow >> power;
 
-    std::cout << flag << " " << power << std::endl;
+    if(mf_flag || monitoring)
+      handle(power);
+
+    std::cout << mf_flag << " " << power << std::endl;
   }
 
   return 1;
