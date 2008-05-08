7c4728ce61dc255e59636b9b36aab876b1eb2d22
gnychis <gnychis@14b32a93-68d6-dc11-9d15-00e08143843c>
2008-05-08 18:52:30 +0000

    trying to output tx

--- tx_quality/test_ack_generator.cc
+++ tx_quality/test_ack_generator.cc
@@ -6,14 +6,17 @@
 #include <map>
 #include <fstream>
 #include <list>
+#include <stdlib.h>
 
 const int TSAMPLES=23872;
-const int COMP_WINDOW=100;
+const int COMP_WINDOW=50;
 const int NSKIP=5000;
 const int POWER_THRESH=580;
 const int MIN_SAMPLES=10000;
+const int SHISTORY_WINDOW=300;
 
 std::list<long> power_history;
+std::list<long> shistory;
 
 enum state_t {
   IDLE,
@@ -22,12 +25,36 @@ enum state_t {
 };
 state_t curr_state;
 
-long tx_average;
+enum result_t {
+  PASS,
+  FAIL,
+  FP
+};
+result_t result;
+
 long curr_average;
 long samples_left;
 long ntransmissions;
 long tx_max;
 long curr_max;
+
+const float PTHRESH=0.13;
+
+std::ofstream ofile;
+
+inline std::string stringify(long x)
+{
+  std::ostringstream o;
+  o << x;
+  return o.str();
+} 
+
+void print_history()
+{
+  std::list<long>::iterator power_it;
+  for(power_it = shistory.begin(); power_it != shistory.end(); power_it++) 
+    ofile << *power_it << std::endl;
+}
 
 void check_power(long mf_flag, long power)
 {
@@ -38,9 +65,10 @@ void check_power(long mf_flag, long power)
     case IDLE:
       if(mf_flag==1) {
         samples_left=TSAMPLES;
-        tx_average=curr_average;
         tx_max=curr_max;
         curr_state=MONITORING;
+        ofile.open("/tmp/out");
+        print_history();
       }
     break;
 
@@ -51,55 +79,73 @@ void check_power(long mf_flag, long power)
 //        samples_left+=NSKIP;
 //        curr_state=SKIPPING;
 //      } 
-      if(power>(tx_max+(0.18*tx_max))) {
-        std::cout << ntransmissions++ << " fail\n";
+      if(power>(tx_max+(PTHRESH*tx_max))) {
+        std::cout << "fail " << ntransmissions++ << std::endl;
+        result=FAIL;
         samples_left+=NSKIP;
         curr_state=SKIPPING;
       }
-      if(power < 100) {
+      if(power < 50) {
         std::cout << " false positive\n";
+        result=FP;
         samples_left=0;
         curr_state=IDLE;
       }
       samples_left--;
       if(samples_left==0) {
-        std::cout << ntransmissions++ <<  " success\n";
+        std::cout << "pass " << ntransmissions++ << std::endl;
+        result=PASS;
         curr_state=SKIPPING;
         samples_left=NSKIP;
       }
+    fflush(stdout);
     break;
 
     case SKIPPING:
-      if(samples_left>0)
+      if(samples_left>0) {
         samples_left--;
-      else 
-        curr_state=IDLE;
+      } else { 
+        curr_state=IDLE; {
+        ofile.close();
+        std::string txs = stringify(ntransmissions-1);
+        if(result==PASS || result==FAIL)
+          system(std::string("mv /tmp/out /home/gnychis/school/gr/cmu_sdrg/tx_quality/tx_"+txs).c_str());
+        if(result==FP)
+          system(std::string("mv /tmp/out /home/gnychis/school/gr/cmu_sdrg/tx_quality/fail_"+txs).c_str());
+        }
+      }
     break;
 
     default:
     break;
   }
+  if(ofile.is_open())
+    ofile << power << std::endl;
 }
 
 void compute_average(long power)
 {  
   power_history.push_back(power);
+  shistory.push_back(power);
 
   if(power_history.size()<COMP_WINDOW) 
     return;
 
-  power_history.pop_front();
+  if(shistory.size()<SHISTORY_WINDOW)
+    return;
 
-  long long sum;
+  power_history.pop_front();
+  shistory.pop_front();
+
   std::list<long>::iterator power_it;
   curr_max=0;
   for(power_it = power_history.begin(); power_it != power_history.end(); power_it++) {
-    sum += *power_it;
+//    sum += *power_it;
     if(*power_it>curr_max)
       curr_max=*power_it;
   }
 
-  curr_average = sum/COMP_WINDOW;
+//  curr_average = sum/COMP_WINDOW;
 }
 
 int main(int argc, char **argv) {
