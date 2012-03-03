7f93ef3783e69a435761bb8b5ace528d5193d382
George Nychis <gnychis@gmail.com>
2012-03-03 16:55:51 -0600

    size output

--- fft_test/test_fft.py
+++ fft_test/test_fft.py
@@ -19,16 +19,18 @@ class my_top_block(gr.top_block):
         # Constructing a sine source and the fft blocks
         src0 = gr.sig_source_c (sample_rate, gr.GR_SIN_WAVE, 350, ampl)
         ss2v = gr.stream_to_vector(gr.sizeof_gr_complex, self._fft_length)
+        sv2s = gr.vector_to_stream(gr.sizeof_gr_complex, self._fft_length)
         fft_demod = gr.fft_vcc(self._fft_length, True, win2, False)
         ifft = gr.fft_vcc(self._fft_length, False, win, False)
+        scale = gr.multiply_const_cc(1.0 / self._fft_length)
         
         # Some output data files
-        trans_output = gr.file_sink(gr.sizeof_gr_complex*self._fft_length, "trans_output.dat")
-        reg_output = gr.file_sink(gr.sizeof_gr_complex*self._fft_length, "reg_output.dat")
+        trans_output = gr.file_sink(gr.sizeof_gr_complex, "trans_output.dat")
+        reg_output = gr.file_sink(gr.sizeof_gr_complex, "reg_output.dat")
 
         # make the connections #
-        self.connect(src0, ss2v, fft_demod, ifft, trans_output)
-        self.connect(ss2v, reg_output)
+        self.connect(src0, ss2v, fft_demod, ifft, sv2s, scale, trans_output)
+        self.connect(src0, reg_output)
 
 if __name__ == '__main__':
     try:
